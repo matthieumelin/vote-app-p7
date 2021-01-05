bc0d3993fab49926d0fae1360774a8a9e9616211
BuildTools <matthieumelin62@gmail.com>
2021-01-06 00:30:15 +0100

    edit polls

--- src/App.js
+++ src/App.js
@@ -7,53 +7,51 @@ import "./App.css";
 import Question from "./components/Question";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      polls: [
-        {
-          id: 1,
-          title: "Abdel est beau ?",
-          answers: [
-            { name: "Oui", votes: 1 },
-            { name: "Non", votes: 0 },
-            { name: "Trop noir", votes: 0 }
-          ]
-        },
-        {
-          id: 2,
-          title: "Vive POPSchool ?",
-          answers: [
-            { id: 1, name: "Oui", votes: 0 },
-            { id: 2, name: "Non", votes: 0 },
-            { id: 3, name: "Plus d'argent", votes: 2 }
-          ]
-        },
-        {
-          id: 3,
-          title: "Le coca c'est très bon ?",
-          answers: [
-            { id: 1, name: "Oui", votes: 0 },
-            { id: 2, name: "Non", votes: 0 },
-            { id: 3, name: "Ya pas que le coca", votes: 2 }
-          ]
-        }
-      ]
-    };
-  }
+        this.state = {
+            polls: [{
+                    id: 1,
+                    title: "Question 1",
+                    answers: [
+                        { name: "Oui", votes: 1 },
+                        { name: "Non", votes: 0 },
+                    ]
+                },
+                {
+                    id: 2,
+                    title: "Question 2",
+                    answers: [
+                        { id: 1, name: "Oui", votes: 0 },
+                        { id: 2, name: "Non", votes: 0 },
+                    ]
+                },
+                {
+                    id: 3,
+                    title: "Question 3",
+                    answers: [
+                        { id: 1, name: "Oui", votes: 0 },
+                        { id: 2, name: "Non", votes: 0 },
+                    ]
+                }
+            ]
+        };
+    }
 
-  render() {
-    const polls = this.state.polls.map(poll => {
-      return <Question key={poll.id} data={poll} handleSubmit={this.vote} />
-    })
+    render() {
+        const polls = this.state.polls.map(poll => {
+            return <Question key = { poll.id }
+            data = { poll }
+            handleSubmit = { this.vote }
+            />
+        })
 
-    return (
-      <div className="container">
-        {polls}
-      </div>
-    );
-  }
+        return ( <
+            div className = "container" > { polls } <
+            /div>
+        );
+    }
 }
 
 export default App;
